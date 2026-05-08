2bb35cd25360fc37d4044faf11bc4ff3b4141ba6
원아카데미 <primx@gmail.com>
2026-05-08 11:11:06 +0900

    바로가기 추가

---
+++ app/src/main/java/app/vercel/oneacademy_app/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.oneacademy_app.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
